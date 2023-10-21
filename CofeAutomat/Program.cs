b26f3f559f13b5d016d63108632d9bc6bdc6560a
AlexeiKonev <alexkonev777@gmail.com>
2023-10-21 15:32:17 +0300

    adding espresso

--- CofeAutomat/Program.cs
+++ CofeAutomat/Program.cs
@@ -17,6 +17,25 @@ namespace CofeAutomat
      * 
      * 
      */
+    /// <summary>
+    /// бъекты должны быть взаимо-
+//    заменяемы с Beverage, поэтому
+//    расширяем класс Beverage.
+//    Также все декораторы долж-
+//ны заново реализовать метод
+//    getDescription(). Зачем? Скоро
+//    узнаете...
+//    Объект Beverage, кото-
+//рый будет «заворачиваться»
+//в каждый Decorator.Обрати-
+//те внимание: мы используем
+//    подтип Beverage, чтобы деко-
+//ратор мог быть оберткой для
+//    любого напитка.
+//    128
+//глава 3
+//Программируем классы напитков
+     /// </summary>
     public abstract class Beverage
     {
      public   string description = "Unknown Beverage";
@@ -27,7 +46,17 @@ namespace CofeAutomat
         public abstract double cost();
     }
 
-
+    /// <summary>
+    /// Все классы конкретных напит- ков расширяют Beverage.
+//    Описание задается в конструкто-
+//ре класса.Стоит напомнить, что
+//переменная description наследуется
+//от Beverage.
+//Остается вычислить стоимость напитка. В этом
+//классе беспокоиться о дополнениях не нужно, поэто-
+//му мы просто возвращаем стоимость «базового»
+//эспрессо — $1.99.
+    /// </summary>
     public class Espresso : Beverage
     {
     public Espresso()
@@ -41,4 +70,23 @@ namespace CofeAutomat
             return 1.99;
         }
     }
+
+    /// <summary> Другой класс напитка. От нас
+//    требуется лишь назначить подхо-
+//дящее описание и вернуть правиль-
+//ную стоимость.
+//Два других класса напитков (DarkRoast
+//и Decaf) создаются аналогично.
+    /// </summary>
+    public class HouseBlend : Beverage
+    {
+public HouseBlend()
+    {
+        description = "House Blend Coffee";
+    }
+    public override double cost()
+    {
+        return 0.89;
+    }
+}
 }
