47c5704263cd89aede875dad19acead1f8cb7bd9
AlexeiKonev <alexkonev777@gmail.com>
2023-10-21 15:20:16 +0300

    Добавьте файлы проекта.

---
+++ CofeAutomat/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CofeAutomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /*classes
+     * 
+     * 
+     */
+    public abstract class Beverage
+    {
+     public   string description = "Unknown Beverage";
+        public string getDescription()
+        {
+            return description;
+        }
+        public abstract double cost();
+    }
+
+
+    public class Espresso : Beverage
+    {
+    public Espresso()
+    {
+        description = "Espresso";
+    }
+   
+
+        public override double cost()
+        {
+            return 1.99;
+        }
+    }
+}
